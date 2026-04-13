172d24a4953cb86d857b77e877c9a34b21120ac9
JustusHenke <119817909+JustusHenke@users.noreply.github.com>
2026-04-13 15:36:55 +0200

    chore: Version 0.12.6.2, Datum aktualisiert, Fallback synchronisiert

--- QCA_AID_assets/__version__.py
+++ QCA_AID_assets/__version__.py
@@ -3,8 +3,8 @@ QCA-AID Version Information
 ===========================
 """
 
-__version__ = "0.12.5"
-__version_date__ = "2026-01-27"
+__version__ = "0.12.6.2"
+__version_date__ = "2026-04-13"
 __author__ = "Justus Henke"
 __contact__ = "justus.henke@hof.uni-halle.de"
 __repository__ = "https://github.com/JustusHenke/QCA-AID"
